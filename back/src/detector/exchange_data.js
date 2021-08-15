7b2f677a798cfd03f2edfb55fd50d8ad38821367
João Rafael <joaorafaelx@gmail.com>
2021-08-15 18:01:01 -0300

    Reword Opportunity Calculator

--- back/src/detector/exchange_data.js
+++ back/src/detector/exchange_data.js
@@ -2,67 +2,67 @@ module.exports = {
   "binance": {
     "buyCost": 0.0,  // Hard value
     "sellCost": 0.0,
-    "buyTax": 0,       // Percentage
-    "sellTax": 0
+    "buyTax": 0.0025,       // Percentage
+    "sellTax": 0.0025
   },
   "huobi": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "hitbtc": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "gdax": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "bitstamp": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "poloniex": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "gemini": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "luno": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "bitso": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "therocktrading": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
   "coinmate": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0,
-    "sellTax": 0
+    "buyTax": 0.0025,
+    "sellTax": 0.0025
   },
 };

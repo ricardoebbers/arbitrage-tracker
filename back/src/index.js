624ac6df94c47ad6a401f7d79da709602d9406cc
João Rafael <joaorafaelx@gmail.com>
2021-08-15 21:14:55 -0300

    Switch back's Rabbit for rx-amqplib

--- back/src/index.js
+++ back/src/index.js
@@ -1,53 +1,41 @@
 const WebSocketServer = require('ws').Server;
-const Rabbit = require('./rabbit');
+const rxAmqp = require('rx-amqplib');
 const OpportunityDetector = require("./detector");
 require('dotenv').config();
 
 const WS_PORT = process.env.WS_PORT;
 const WS_PATH = process.env.WS_PATH;
-
-
-const DEBUG = false;
+const QUEUE_NAME = process.env.QUEUE_NAME;
+const RABBITMQ_URL = process.env.RABBITMQ_URL;
 
 const listeners = []
-let rabbitSubscribed = false;
 
-async function startServer() {
-  wss = new WebSocketServer({ port: WS_PORT, path: WS_PATH });
-  const rabbit = await (new Rabbit()).initialize();
+console.log("Starting WS server...")
+wss = new WebSocketServer({ port: WS_PORT, path: WS_PATH });
+wss.on('connection', async function(ws) {
+  listeners.push(ws);
+  ws.on("close", () => {
+    listeners.splice(listeners.indexOf(ws), 1);
+  })
+});
 
-  wss.on('connection', async function(ws) {
-    process.stdout.write("*")
-    listeners.push(ws);
-
-    if (!rabbitSubscribed) {   
-      rabbitSubscribed = true;
-      rabbit.subscribe((msg) => {
-        onEvent(msg);
-      })
-    }
-
-    ws.on("close", () => {
-      process.stdout.write("x")
-      listeners.splice(listeners.indexOf(ws), 1);
-    })
-  });
-}
+console.log("Connecting to Rabbit...")
+rxAmqp.newConnection(RABBITMQ_URL)
+  .flatMap(connection => connection.createChannel())
+  .flatMap(channel => channel.assertQueue(QUEUE_NAME, { durable: true }))
+  .flatMap(reply => reply.channel.prefetch(59))
+  .flatMap(reply => reply.channel.consume(QUEUE_NAME, { noAck: true }))
+  .doOnNext(msg => onEvent(msg))
+  .subscribe();
 
 async function onEvent(msg) {
-  process.stdout.write(".")
   if (!msg) return;
   try {
     const message = JSON.parse(msg.content.toString())
-    if (!message.events) return process.stdout.write("?");
-    if (DEBUG) console.log(message.events);
+    if (!message.events) return console.log("Invalid message: "  + message);
     message.opportunities = OpportunityDetector(message.events)
     listeners.forEach(ws => ws.send(JSON.stringify(message)))
   } catch (e) {
     console.log(e)
-    process.stdout.write("!")
   }
 }
-
-startServer()
-console.log('Server started')

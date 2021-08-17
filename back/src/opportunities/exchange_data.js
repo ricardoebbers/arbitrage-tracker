db5a4ec7e6f6c62e6d28b9fdfca2066f9f1eb0bd
João Rafael <joaorafaelx@gmail.com>
2021-08-17 01:52:52 -0300

    Lower transaction taxes

--- back/src/opportunities/exchange_data.js
+++ back/src/opportunities/exchange_data.js
@@ -2,67 +2,67 @@ module.exports = {
   "binance": {
     "buyCost": 0.0,  // Hard value
     "sellCost": 0.0,
-    "buyTax": 0.0025,       // Percentage
-    "sellTax": 0.0025
+    "buyTax": 0.00125,       // Percentage
+    "sellTax": 0.00125
   },
   "huobi": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "hitbtc": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "gdax": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "bitstamp": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "poloniex": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "gemini": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "luno": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "bitso": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "therocktrading": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
   "coinmate": {
     "buyCost": 0,
     "sellCost": 0,
-    "buyTax": 0.0025,
-    "sellTax": 0.0025
+    "buyTax": 0.00125,
+    "sellTax": 0.00125
   },
 };
